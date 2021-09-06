050646270031a2fc54908dd94c3be83771da247a
M Natoli <natoli.missy@gmail.com>
2021-09-05 21:05:44 -0500

    styled home and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,21 +2,21 @@ import { SocialIcon } from "react-social-icons";
 
 function Footer() {
     return (
-        <footer>
-            <div className="socials">
-                <div>
+        <footer className="relative bg-gray-400">
+            <div className="inset-x-0 flex justify-between items-center mx-0.5 md:justify-center">
+                <div className="md:px-5">
                     <SocialIcon
                         url="https://www.linkedin.com/in/melissa-natoli/"
                         target="_blank"
                     />
                 </div>
-                <div>
+                <div className="md:px-5">
                     <SocialIcon
                         url="https://github.com/misn0147"
                         target="_blank"
                     />
                 </div>
-                <div>
+                <div className="md:px-5">
                     <SocialIcon
                         url="https://soundcloud.com/user-693992123"
                         target="_blank"
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,28 @@
 import React from "react";
+import austin from "../../assets/austin.jpg";
+import austinSmall from "../../assets/austin-small.JPG";
 
 function Home() {
     return (
         <section>
-            <div className="hero"></div>
-            <div className="social-line">
-                <h1 className="titles">
-                    <u>Melissa Natoli</u>
-                </h1>
-                <h2 className="titles">Austin, Texas based Web Developer</h2>
+            <div>
+            
+            <div className="bg-cover bg-center">
+            <div className="responsive inset-x-0 top-20 flex justify-center items-center">
+                    <h2 className="md:text-3xl font-sans">Austin, Texas based Web Developer</h2>
+                </div>
+                <img
+                    src={austin}
+                    className="min-w-full hidden md:block"
+                    alt="austin skyline over lake"
+                />
+                <img
+                    src={austinSmall}
+                    className="min-w-full md:hidden"
+                    alt="austin skyline over lake"
+                />
+                
+            </div>
             </div>
         </section>
     );
