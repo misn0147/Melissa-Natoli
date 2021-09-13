1561c25fcb27fb67571c0e7a0b12058672557e9f
M Natoli <natoli.missy@gmail.com>
2021-09-13 12:07:03 -0500

    changed instructions wording

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -12,11 +12,11 @@ const Instructions = (props) => {
         <Container fluid className="shadow-box p-2 max-w-95 md:max-w-3/4 mx-auto flex-col justify-center text-center bg-gray-500">
             <h2 className="mt-2 text-xs md:text-base text-lime-50">
                 Thanks for checking out my portfolio! While you're here try to
-                beat the high score!
+                win the game!
             </h2>
             <p className="text-xs md:text-base text-lime-50">
                 Click on a photo to earn a point, but don't click a photo more
-                than once or the game will be over!
+                than once or the game will be over! 12 points wins the game!
             </p>
             {/* show message from props  */}
             <h5 className={userMsg}>{props.userMessage}</h5>{" "}
